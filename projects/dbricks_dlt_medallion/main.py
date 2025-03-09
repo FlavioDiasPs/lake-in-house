fb72ab1c14b84a4ffb988b8b86d1464dc368e060
Flavio Pegas <flaviodiasps@gmail.com>
2025-03-09 19:34:45 -0300

    Add Databricks DLT for deposit using vscode extension

--- projects/dbricks_dlt_medallion/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from dbricks-dlt-medallion!")
-
-
-if __name__ == "__main__":
-    main()
--- projects/dbricks_dlt_medallion/tests/main_test.py
+++
@@ -1,6 +0,0 @@
-from dbricks_dlt_medallion.main import get_taxis, get_spark
-
-
-def test_main():
-    taxis = get_taxis(get_spark())
-    assert taxis.count() > 5
