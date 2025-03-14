195c91994a6c281468410aff7af06704941781b4
Flavio Pegas <flaviodiasps@gmail.com>
2025-03-13 23:50:19 -0300

    Added: Column Mask

--- projects/sql_dlt/src/sql_pipeline_medalion/pipelines/4-gold_dynamic_views/vw_fct_transaction_pipeline.sql
+++ projects/sql_dlt/src/sql_pipeline_medalion/pipelines/4-gold_dynamic_views/vw_fct_transaction_pipeline.sql
@@ -1,16 +1,27 @@
 USE SCHEMA GOLD;
 
 CREATE OR REFRESH MATERIALIZED VIEW vw_fct_transaction_pipeline
-TBLPROPERTIES(pipelines.channel = "PREVIEW")
-WITH ROW FILTER lab.filters.even_filter on (id)
+(
+  date_partition DATE                                 COMMENT 'The computed date to use as partition on the storage',
+  id FLOAT                                            COMMENT 'Unique monotonicaly increasing primary key of deposit table',
+  amount DECIMAL(38,18)                               COMMENT 'The value deposited',
+  user_id STRING                                      COMMENT 'The Id of the user in the app',
+  currency STRING MASK lab.filters.currency_mask      COMMENT 'The currency string of the value deposited',
+  ts_event TIMESTAMP                                  COMMENT 'The time which the deposit happenned',
+  tx_status STRING                                    COMMENT 'The status of the transaction (complete, failed)',
+  interface STRING                                    COMMENT 'The place where the transaction was made',
+  transaction_type STRING                             COMMENT 'The type of the transaction (deposit, withdrawal)'
+)
+TBLPROPERTIES('pipelines.channel' = 'PREVIEW')
+WITH ROW FILTER lab.filters.even_filter on (id) --row filter https://docs.databricks.com/aws/en/tables/row-and-column-filters
 AS
 SELECT 
-  date_partition,  
+  date_partition,
   id,
   amount,
   CASE WHEN is_account_group_member('invented_group') 
         THEN user_id ELSE 'REDACTED' 
-    END AS user_id,
+    END AS user_id, --dynamic view https://learn.microsoft.com/en-us/azure/databricks/views/dynamic
   currency,
   ts_event,
   tx_status,
